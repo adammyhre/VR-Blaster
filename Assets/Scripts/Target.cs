2ea75f76687580790bf1beda60310dbaddae9b08
Adam Myhre <adam@undergalaxie.com>
2019-06-15 16:32:49 -0700

    Updated target.cs

--- Assets/Scripts/Target.cs
+++ Assets/Scripts/Target.cs
@@ -4,5 +4,62 @@ using UnityEngine;
 
 public class Target : MonoBehaviour
 {
-   
+	public Color flashDamageColor = Color.white;
+
+	private MeshRenderer mesh = null;
+	private Color originalColor = Color.white;
+
+	private int maxHealth = 2;
+	private int health = 0;
+
+
+	private void Awake () {
+		mesh = GetComponent<MeshRenderer> ();
+		originalColor = mesh.material.color;
+	}
+
+	private void OnEnable () {
+		ResetHealth ();
+	}
+
+	private void OnCollisionEnter (Collision collision) {
+		if (collision.gameObject.CompareTag ("Projectile")) {
+			Damage ();
+		}
+	}
+
+	private void Damage(){
+		StopAllCoroutines ();
+		StartCoroutine (Flash());
+		RemoveHealth ();
+	}
+
+	private IEnumerator Flash () {
+		mesh.material.color = flashDamageColor;
+
+		WaitForSeconds wait = new WaitForSeconds (0.1f);
+		yield return wait;
+
+		mesh.material.color = originalColor;
+	}
+
+	private void RemoveHealth (){
+		health--;
+		CheckForDeath ();
+	}
+
+	private void ResetHealth () {
+		health = maxHealth;
+	}
+
+	private void CheckForDeath (){
+		if (health <= 0)
+			Kill ();
+	}
+
+	private void Kill () {
+		gameObject.SetActive (false);
+	}
+
+
 }
